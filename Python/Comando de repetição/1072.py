65060e233c6a70bb69d5ee0ae3ddfca313a90255
Luísa Lima <79111102+luisa0602@users.noreply.github.com>
2021-09-15 08:38:02 -0300

    Create 1072.py

---
+++ Python/Comando de repetição/1072.py
@@ -0,0 +1,14 @@
+n=int(input())
+a=[]
+inn=0
+out=0
+for c in range(0,n):
+  a.append(int(input()))
+for c in a:
+  if 10<=c<=20:
+    inn+=1
+  elif c<10 or c>20:
+    out+=1
+
+print('{} in'.format(inn))
+print('{} out'.format(out))
